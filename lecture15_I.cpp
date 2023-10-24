6e31c1e31380695acc3459d3fdef080325e18c59
Abhijeet Singh <116811157+Abhijeet14d@users.noreply.github.com>
2023-10-24 14:13:56 +0530

    Add files via upload

---
+++ lecture15_I.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    // char r,o,h,i,t;
+    // cin >> r >> o >> h >> i >> t;
+
+    // char name[5];
+
+    // for(int i=0;i<5;i++){
+    //     cin >> name[i];
+    // }
+
+
+    // cout << "the name you just entered is: "<< endl;
+
+    // for(int i=0;i<5;i++){
+    //     cout << name[i];
+    // }
+    // cout << endl;
+
+    // char name[1000];
+
+    // cout << "enter your name: (note : Enter # after the end of your name: )";
+
+    // int i=0;
+    // while (1)
+    // {
+    //     cin >> name[i];
+    //     if(name[i]=='#') break;
+    //     i++;
+    // }
+    
+    // cout << "the name you just entered is: "<< endl;
+    // i=0;
+    // while (name[i]!='#')
+    // {
+    //     cout << name[i];
+    // }
+
+    string name;
+
+    cin >> name;
+    cout << name;
+    
+
+    return 0;
+}
