e07799d5cd904f4f9208193d5313b5636e5ebdbb
Abhijeet Singh <116811157+Abhijeet14d@users.noreply.github.com>
2023-10-24 14:30:37 +0530

    Add files via upload

---
+++ lecture15_II.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // string name;
+
+    // cin >> name;
+
+    // int i;
+    // for(i=0;i<5;i++){
+    //     cout << name[i] << endl;
+    // }
+
+    // string a;
+    // string b;
+
+    // //int a,b;
+
+    // cin >> a >> b;
+    // cout << "the strings which you have given as input are: "<< a << b << endl;
+
+    // string c;
+    // // int c;
+    // c = a+b; // concatination.
+    // cout << "the combined word is: "<< c << endl;
+    // cout << "the length of the combined name is: "<<c.length()<< endl;
+
+
+    string a;
+    getline(cin,a);
+    cout << "your full name is: "<<endl;
+
+
+    return 0;
+}
